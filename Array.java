b3598027f3557495cc453a22730669b33b853c12
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:39:48 -0300

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,22 @@
+package explicacoes;
+
+public class Array {
+
+	public static void main(String[]args) {
+		
+		String [] nome= {"Edu","Jailson","Geovanna","Andressa","Wesley"};
+		nome [0] = "Clóvis";
+		int[] numero = {12,7,94,88,0};
+		
+		System.out.println(nome[3]);
+		System.out.println(numero[3]);
+		System.out.println(nome[0]);
+		
+		System.out.println(nome.length);
+		System.out.println(numero.length);
+		
+			
+		}
+		
+	}
+
