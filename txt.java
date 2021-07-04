305b4f6ca0f4dead9701bdd9d641076220a18b18
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:52:24 -0300

    Create txt.java

---
+++ txt.java
@@ -0,0 +1,10 @@
+package explicacoes;
+
+public class txt {
+
+	public static void main(String[] args) {
+		String txt = "Andressa";
+		System.out.println(txt.toUpperCase());
+		System.out.println(txt.toLowerCase());
+	}
+}
