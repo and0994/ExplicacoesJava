3d10efb57c0c64ceaf563aeacbfd1b78e15f43ab
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:51:01 -0300

    Create PrimeiroProgramaJava.java

---
+++ PrimeiroProgramaJava.java
@@ -0,0 +1,10 @@
+package explicacoes;
+
+public class PrimeiroProgramaJava {
+
+	public static void main(String[]args) {
+		
+		System.out.println("Hello World"); /**ln serve para pular linha, tambem pode usar \n dento das ""*/
+		System.out.println("Andressa");
+	}
+}
