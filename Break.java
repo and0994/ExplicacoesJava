be20bc2e68739f71ad49d9c0c3ba32c0c5dcb38b
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:41:49 -0300

    Create Break.java

---
+++ Break.java
@@ -0,0 +1,14 @@
+package explicacoes;
+
+public class Break {
+
+	public static void main(String[] args) {
+		for(int i=0; i<11; i++) {
+			if(i==7) {
+				break;
+			}
+			System.out.println(i);
+		}
+		
+	}
+}
