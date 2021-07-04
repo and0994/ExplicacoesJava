bfa693b157fc8cb20d7c6ba1261455dd613e5d84
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:47:51 -0300

    Create LacoDoWhile.java

---
+++ LacoDoWhile.java
@@ -0,0 +1,16 @@
+package explicacoes;
+
+import java.awt.Container;
+//REPITA
+public class LacoDoWhile {
+// testa no final, então mesmo sendo falso ele imprime uma vez
+	public static void main(String[] args) {
+		int cont=0;
+		do {
+			System.out.println("Andressa");
+			cont++;
+		} while (cont < 10);
+
+	}
+
+}
