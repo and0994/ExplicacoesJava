8b8be645f2c78d9ffb4cc4e43068af190c282825
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:49:33 -0300

    Create LacoFor03.java

---
+++ LacoFor03.java
@@ -0,0 +1,11 @@
+package explicacoes;
+
+public class LacoFor03 {
+// laco com de repetição com decremento
+	public static void main(String[]args) {
+		
+		for(int i=100; i>0; i--) {
+			System.out.println(i);
+		}
+	}
+}
