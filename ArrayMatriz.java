9e3480d93b2a4e4f8a0c174bef4fdb896fd6b5e7
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:40:50 -0300

    Create ArrayMatriz.java

---
+++ ArrayMatriz.java
@@ -0,0 +1,12 @@
+package explicacoes;
+
+public class ArrayMatriz {
+
+	public static void main(String[] args) {
+		
+		int [][] numero= {{10, 12, 14,17},{4, 5, 6}, {7, 9, 11, 23}};
+		
+		int x = numero[2][2];
+		System.out.println(x);
+	}
+}
