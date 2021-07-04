414589ee54b6985794e2397a9d772fb23e1c77dd
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:42:45 -0300

    Create Concatenacao.java

---
+++ Concatenacao.java
@@ -0,0 +1,10 @@
+package explicacoes;
+
+public class Concatenacao {
+	public static void main(String[] args) {
+		String nome="Andressa";
+		String sobrenome="Oliveira";
+		
+		System.out.println("O nome completo "+nome+" "+sobrenome);
+	}
+}
