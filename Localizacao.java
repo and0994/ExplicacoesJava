2a60b4e92013ca59613e38537af8cf693273edba
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:50:28 -0300

    Create Localizacao.java

---
+++ Localizacao.java
@@ -0,0 +1,20 @@
+package explicacoes;
+
+public class Localizacao {
+
+	public static void main(String[] args) {
+		String paragrafo = "Lorem ipsum accumsan metus condimentum hendrerit amet iaculis feugiat augue platea eu, class sociosqu aptent adipiscing cubilia tortor faucibus hendrerit aliquam. nisl fames tempor blandit adipiscing primis id sem commodo, nunc praesent nibh curabitur adipiscing porta mauris praesent etiam, tincidunt integer fames habitasse lobortis suspendisse tortor. scelerisque donec vestibulum lacus placerat adipiscing varius semper curabitur, consequat pulvinar in commodo torquent enim tempor venenatis quam, consequat integer blandit fusce vulputate massa proin. gravida mollis suspendisse varius vitae aliquam erat senectus egestas consequat tempor fermentum tellus taciti integer, morbi semper suspendisse imperdiet cubilia libero tellus class lobortis inceptos torquent pellentesque etiam. \r\n"
+				+ "\r\n"
+				+ "	Nibh sit metus erat quam sociosqu libero placerat consequat netus, blandit quisque lectus ornare erat volutpat convallis dolor molestie, donec platea elementum mauris sed curabitur dictum varius. magna hac aenean phasellus nulla morbi tempus eros vel etiam, sodales torquent euismod cras curabitur arcu donec per. ad curae eleifend pretium himenaeos aptent aliquam mollis sagittis curabitur vitae ultricies primis, posuere litora tincidunt aliquam pellentesque sodales vestibulum curabitur sed venenatis. elementum potenti nam enim eget etiam ac primis lectus elementum metus curae, pulvinar nisl hac nisl convallis in sociosqu et eros risus, nec feugiat volutpat tempus facilisis orci sollicitudin lobortis ultricies laoreet. \r\n"
+				+ "\r\n"
+				+ "	Aenean tortor quisque ad aptent mi convallis quisque lectus ornare enim ipsum leo risus viverra porta, aliquam habitant nisl vulputate litora euismod porta orci hendrerit diam volutpat amet sed facilisis. amet conubia mi amet ullamcorper eros porta luctus non cursus himenaeos fusce leo, netus magna blandit faucibus pellentesque quisque fusce ligula scelerisque ultrices odio, blandit ipsum dui nec integer donec torquent integer cubilia dapibus posuere. vulputate nullam mauris aenean scelerisque blandit habitasse aenean fames torquent lacus, nulla duis eget luctus cursus nibh nullam dictumst vivamus, lorem sollicitudin rutrum scelerisque erat lectus proin class quam. \r\n"
+				+ "\r\n"
+				+ "	Nisl viverra at duis magna fermentum at feugiat enim ultrices massa netus feugiat, nostra facilisis amet magna adipiscing vehicula laoreet nam fusce sagittis. nec congue dolor aptent ad integer quisque netus quam, sociosqu aliquam erat proin maecenas ipsum suspendisse sociosqu lectus, suscipit posuere hendrerit dapibus nunc porta inceptos. vivamus dictum est enim placerat vitae cras elit turpis quam, aenean ac faucibus pretium etiam interdum eros ultrices, lobortis tempor aliquam suspendisse donec libero eros ante. donec ligula inceptos habitasse aenean nunc porta vitae lobortis, elementum vivamus curabitur scelerisque platea sollicitudin aliquet. \r\n"
+				+ "\r\n"
+				+ "	Hendrerit congue amet quam nunc porta curae a integer, egestas fames pellentesque varius risus a lorem blandit fames, lectus nibh dui est primis enim duis. porttitor elit lobortis quisque massa laoreet metus ligula sit sem curabitur ut placerat sit, mattis aenean scelerisque curabitur auctor aliquam dapibus interdum torquent elementum amet netus. non faucibus nibh odio in orci vehicula euismod nostra placerat, quam purus vestibulum etiam quis neque sodales tellus feugiat nulla, luctus vel felis lobortis ad lorem pharetra mi. himenaeos sit interdum volutpat pellentesque quisque consectetur vivamus, neque in pharetra eros non faucibus placerat viverra, cursus litora commodo ullamcorper odio ante. \r\n"
+				+ "\r\n"
+				+ "	Pretium risus congue fusce mollis morbi, etiam pretium nunc dictumst convallis est, quisque sed id metus. ";
+
+		System.out.println("A quatidade da caracteres antes da palavra é:" + paragrafo.indexOf("ultricies"));
+	}
+}
