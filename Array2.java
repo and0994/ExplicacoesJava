7364ac1b6d51899ab6455cb383aa9c2f18183dfb
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:40:17 -0300

    Create Array2.java

---
+++ Array2.java
@@ -0,0 +1,14 @@
+package explicacoes;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		String [] pais = {"Japão"," Brasil", " Colombia", " Dinamarca", " Alemanha", " Nigéria", " Israel", " Tailândia"," Chile", " Grécia"};
+	
+		for(String i: pais);{
+
+			System.out.print(i+"\n");
+		}
+		
+	}
+}
