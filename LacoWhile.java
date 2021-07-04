6f8e30d29fded9b4fcd3e07979186f94950f42cb
Andressa Costa <79861793+and0994@users.noreply.github.com>
2021-07-04 17:49:58 -0300

    Create LacoWhile.java

---
+++ LacoWhile.java
@@ -0,0 +1,13 @@
+package explicacoes;
+
+public class LacoWhile {
+
+	public static void main(String[] args) {
+		
+		int cont=0;
+		while(cont<9) {
+		System.out.println("cont");
+		cont=cont+1;
+		}
+	}
+}
